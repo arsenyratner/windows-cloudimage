5e2968538a5c7fd83983a55e271f4720f03e8e02
aratner <aratner@croc.ru>
2025-03-06 17:52:14 +0300

    1

--- w10/UnattendResources/CustomScripts/RunBeforeSysprep.ps1
+++ w10/UnattendResources/CustomScripts/RunBeforeSysprep.ps1
@@ -1,3 +1,11 @@
 #Удаляем приложения которые мешают запечатать ОС
 # Get-AppxPackage -all AD2F1837.HPPrinterControl | Remove-AppxPackage -AllUsers
-
+& powercfg.exe /x -hibernate-timeout-ac 0
+& powercfg.exe /x -hibernate-timeout-dc 0
+& powercfg.exe /x -disk-timeout-ac 0
+& powercfg.exe /x -disk-timeout-dc 0
+& powercfg.exe /x -monitor-timeout-ac 0
+& powercfg.exe /x -monitor-timeout-dc 0
+& Powercfg.exe /x -standby-timeout-ac 0
+& powercfg.exe /x -standby-timeout-dc 0
+& powercfg.exe /hibernate off
