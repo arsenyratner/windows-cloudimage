32d5bad27f20b16d28378be3f949b3f8e683a8df
aratner <aratner@croc.ru>
2026-03-24 09:14:06 +0300

    1

--- _replace/WinImageBuilder.psm1
+++ _replace/WinImageBuilder.psm1
@@ -389,7 +389,7 @@ function Convert-VirtualDisk {
         [Parameter(Mandatory=$false)]
         [boolean]$CompressQcow2
     )
-
+    Write-Log "Convert-VirtualDisk starting"
     if ( (-not ([string]::IsNullOrEmpty($env:TEMPLATE_DIR_PATH))) -and (Test-Path $env:TEMPLATE_DIR_PATH) ) { 
         # get outPath file name only
         $outFile = (split-path -leaf $outPath)
